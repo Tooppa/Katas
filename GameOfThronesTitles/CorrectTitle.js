081093fe4fe0f7247108a8e969141b54730248ee
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-20 10:43:49 +0200

    FEAT: Game of thrones assignment done.

--- GameOfThronesTitles/CorrectTitle.js
+++ GameOfThronesTitles/CorrectTitle.js
@@ -1,7 +1,15 @@
-const correctTitle = () => {
-    
-}
 
-correctTitle("jOn SnoW, kINg IN thE noRth")
-correctTitle("sansa stark,lady of winterfell.")
-correctTitle("TYRION LANNISTER, HAND OF THE QUEEN.")
+const correctTitle = (str) => {
+    const lowerCase = ['and', 'the', 'of', 'in'];
+    const toLowerAndSplit = (s) => s.toLowerCase().split(' ');
+    const firstToUpper = (w) => w.charAt(0).toUpperCase() + w.slice(1);
+    return toLowerAndSplit(str)
+        .map(w => 
+            lowerCase.includes(w) ? w : firstToUpper(w)
+        ).join(' ');
+}
+//const correctTitle=s=>s.toLowerCase().split(' ').map(w=>['and','the','of','in'].includes(w)?w:w.charAt(0).toUpperCase()+w.slice(1)).join(' ');
+
+console.log(correctTitle("jOn SnoW, kINg IN thE noRth"));
+console.log(correctTitle("sansa stark, lady of winterfell."));
+console.log(correctTitle("TYRION LANNISTER, HAND OF THE QUEEN."));
