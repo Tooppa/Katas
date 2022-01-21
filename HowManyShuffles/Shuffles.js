72221888c1363fdac8a20b21f3507ae4148ff4d5
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-21 11:15:44 +0200

    Shortened shuffles

--- HowManyShuffles/Shuffles.js
+++ HowManyShuffles/Shuffles.js
@@ -3,19 +3,15 @@ const shuffleCount = (size) => {
     let suffled = array;
     const halfSize = size / 2;
     let suffles = 0;
-
     const suffle = () => {
-        let firstHalf = suffled.slice(0,halfSize);
-        let secondHalf  = suffled.slice(halfSize);
         let temp = []
         for (i = 0; i < halfSize; i++) {
-            temp.push(firstHalf[i])
-            temp.push(secondHalf[i])
+            temp.push(suffled[i])
+            temp.push(suffled[i + halfSize])
         }
         suffled = temp;
         suffles++;
     }
-
     while(true){
         suffle();
         if (suffled.toString() === array.toString())
