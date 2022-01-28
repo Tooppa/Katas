16a1eefa5bb6fce74363d48335b49e6740b5c283
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-28 10:15:03 +0200

    caeser base

---
+++ CaesersCypher/encode.js
@@ -0,0 +1,9 @@
+const caesarCipher = (str) => {
+    let encoded = '';
+    console.log(encoded)
+}
+
+caesarCipher("Always-Look-on-the-Bright-Side-of-Life", 5)
+//"Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"
+caesarCipher("A friend in need is a friend indeed", 20)
+//"U zlcyhx ch hyyx cm u zlcyhx chxyyx"
