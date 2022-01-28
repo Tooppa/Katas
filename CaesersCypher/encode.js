c26e1acb14dc29a23b1fb6d8a9b104000812df66
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-28 11:18:00 +0200

    caeser cypher works

--- CaesersCypher/encode.js
+++ CaesersCypher/encode.js
@@ -1,8 +1,27 @@
-const caesarCipher = (str) => {
+const caesarCipher = (str, rotation) => {
     let encoded = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const newMethodName = (c, chars) => {
+        if (chars.includes(c)) {
+            let newIndex;
+            if (chars.indexOf(c) + rotation < chars.length)
+                newIndex = chars.indexOf(c) + rotation;
+            else
+                newIndex = chars.indexOf(c) + rotation - chars.length;
+            encoded += chars[newIndex];
+        }
+    };
+    str.split('').forEach((c, index) => {
+        newMethodName(c, alphabet)
+        newMethodName(c, alphabet.toUpperCase())
+        if (encoded.length <= index)
+            encoded += c;
+    });
     console.log(encoded)
 }
 
+caesarCipher("middle-Outz", 2)
+//"okffng-Qwvb"
 caesarCipher("Always-Look-on-the-Bright-Side-of-Life", 5)
 //"Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"
 caesarCipher("A friend in need is a friend indeed", 20)
