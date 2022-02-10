c5968fb3a22d61dbcefc1517023d6bd6d60774e7
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-10 11:20:29 +0200

    C# test

--- HigherNumber/HigherNumber/Program.cs
+++ HigherNumber/HigherNumber/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 class Program
 {
-    void existsHigher(int[] arr, int num)
+    static void Main()
+    {
+        existsHigher(new int[] { 5, 3, 15, 22, 4 }, 10);
+        existsHigher(new int[] { 1, 2, 3, 4, 5 }, 8);
+        existsHigher(new int[] { 4, 3, 3, 3, 2, 2, 2 }, 4);
+        existsHigher(new int[] { -10, -99, -57, -4 }, -4);
+        existsHigher(new int[] { 5 }, 5);
+        existsHigher(new int[] { 99, 99 }, 99);
+        existsHigher(new int[] { }, 5);
+    }
+
+    static void existsHigher(int[] arr, int num)
     {
         foreach (int i in arr)
         {
-
+            Console.WriteLine(i);
         }
-    }
-    void Main()
-    {
-        existsHigher([5, 3, 15, 22, 4], 10);
-        existsHigher([1, 2, 3, 4, 5], 8);
-        existsHigher([4, 3, 3, 3, 2, 2, 2], 4);
-        existsHigher([-10, -99, -57, -4], -4);
-        existsHigher([5], 5);
-        existsHigher([99, 99], 99);
-        existsHigher([], 5);
     }
 }
