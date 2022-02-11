93d3bb933f440155d11bf13055302ce5cc839368
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-11 09:43:46 +0200

    Yesterdays kata now works

--- HigherNumber/HigherNumber/Program.cs
+++ HigherNumber/HigherNumber/Program.cs
@@ -3,20 +3,21 @@ class Program
 {
     static void Main()
     {
-        existsHigher(new int[] { 5, 3, 15, 22, 4 }, 10);
-        existsHigher(new int[] { 1, 2, 3, 4, 5 }, 8);
-        existsHigher(new int[] { 4, 3, 3, 3, 2, 2, 2 }, 4);
-        existsHigher(new int[] { -10, -99, -57, -4 }, -4);
-        existsHigher(new int[] { 5 }, 5);
-        existsHigher(new int[] { 99, 99 }, 99);
-        existsHigher(new int[] { }, 5);
+        Console.WriteLine(existsHigher(new int[] { 5, 3, 15, 22, 4 }, 10));
+        Console.WriteLine(existsHigher(new int[] { 1, 2, 3, 4, 5 }, 8));
+        Console.WriteLine(existsHigher(new int[] { 4, 3, 3, 3, 2, 2, 2 }, 4));
+        Console.WriteLine(existsHigher(new int[] { -10, -99, -57, -4 }, -4));
+        Console.WriteLine(existsHigher(new int[] { 5 }, 5));
+        Console.WriteLine(existsHigher(new int[] { 99, 99 }, 99));
+        Console.WriteLine(existsHigher(new int[] { }, 5));
     }
 
-    static void existsHigher(int[] arr, int num)
+    static bool existsHigher(int[] arr, int num)
     {
         foreach (int i in arr)
         {
-            Console.WriteLine(i);
+            if (i > num) return true;
         }
+        return false;
     }
 }
