914c4e29c33617d4cd34e8822f4bad5c97a530fa
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-11 11:02:33 +0200

    palindrome wip

---
+++ CreatePalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void minPalindromeSteps(string str)
+    {
+        char temp = ' ';
+        int i = 0;
+        foreach (char c in str)
+        {
+            if (c == temp)
+            {
+                Console.WriteLine(str[i]);
+                return;
+            }
+            i++;
+            temp = c;
+        }
+    }
+    static void Main()
+    {
+        minPalindromeSteps("race");
+        minPalindromeSteps("mada");
+        minPalindromeSteps("mirror");
+    }
+}
