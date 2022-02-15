473e493f3202e2169cb0b788a365df6d353b19b7
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-15 10:28:28 +0200

    reverse case

---
+++ ReverserCase/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Reverse(string str)
+    {
+        static char reverseCase(char c) => Char.IsUpper(c) ? Char.ToLower(c) : Char.ToUpper(c);
+        Console.WriteLine(String.Concat(str.Select(c => reverseCase(c))));
+    }
+    static void Main()
+    {
+        Reverse("Happy Birthday");
+        Reverse("MANY THANKS");
+        Reverse("sPoNtAnEoUs"); 
+    }
+}
