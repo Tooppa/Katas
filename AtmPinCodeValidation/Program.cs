749e10d72c751180b0d64c3f5738af2b9e3b8ba1
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-02 10:45:50 +0200

    ATM pin validation

---
+++ AtmPinCodeValidation/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void ValidatePIN(string str)
+    {
+        bool result = true;
+        if (string.IsNullOrEmpty(str) || str.Length > 4 || str.Length < 4) result = false;
+        else foreach (char c in str) if (!char.IsDigit(c)) result = false;
+        Console.WriteLine(result);
+    }
+    public static void Main()
+    {
+        ValidatePIN("1234");// ➞ true
+
+        ValidatePIN("12345");// ➞ false
+
+        ValidatePIN("a234");// ➞ false
+
+        ValidatePIN("");// ➞ false
+    }
+}
